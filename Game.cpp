e99934fdb7afd17d6cee2d51db55a2b2900a2519
Boso101 <bosorevived@gmail.com>
2021-04-19 14:07:19 +0930

    Implement Print  Game State

--- Game.cpp
+++ Game.cpp
@@ -30,6 +30,18 @@ void Game::PrintGameState()
 {
 	// Empty string
 	std::string gameString = "";
+
+	// nested for loop into board
+
+	for (int i = 0; i < board.ROWS - 1; i++)
+	{
+		for (int j = 0; j < board.COLUMNS; j++)
+		{
+
+			gameString << board.GetSpace(j, i);
+		}
+	}
+
 	
 
 
