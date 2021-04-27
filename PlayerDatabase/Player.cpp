f2f2408f43ef549b5246f843f3c2bec4594f6897
Boso101 <bosorevived@gmail.com>
2021-04-27 14:09:43 +0930

    Base main()

--- PlayerDatabase/Player.cpp
+++ PlayerDatabase/Player.cpp
@@ -1,8 +1,11 @@
 #include "Player.h"
+#include <xpolymorphic_allocator.h>
 
 Player::Player(const char* name, unsigned int highScore)
 {
 	this->highScore = highScore;
 
+	//Copy the char* into a char[]
+	strcpy_s(this->playerName, NAME_LENGTH, name);
 
 }
--- PlayerDatabase/PlayerDatabase.cpp
+++ PlayerDatabase/PlayerDatabase.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "**** Player Database ****" << std::endl;
 }
 
 
